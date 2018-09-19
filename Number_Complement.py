abd032723fb4775a6d0b0d56f44d5390ecd6797b
littlelienpeanut <littlelienpeanut@gmail.com>
2018-09-19 16:13:44 +0800

    easy (beats 100% of Python)

--- Number_Complement.py
+++ Number_Complement.py
@@ -1,15 +1,19 @@
 class Solution:
-    def findComplement(self, num):
+    def findComplement(self, nums):
         """
         :type num: int
         :rtype: int
         """
-        b_num = bin(num)[2:]
-        b_num = b_num[::-1]
-        output = 0
+        nums = bin(nums)[2:]
+        output = ''
+        for i in range(len(nums)):
+            if nums[i] == '1':
+                output += '0'
+            
+            else:
+                output += '1'
 
-        for i in range(0, len(b_num), 1):
-            if b_num[i] == '0':
-                output += pow(2, i)
-
-        return output
+        return int(output, 2)
+                
+        
+        
